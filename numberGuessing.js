215e15c8a3a3db2a5224c757024fe697f9ff32c0
Starnoh254 <stajohn697@gmail.com>
2024-03-06 17:48:06 +0300

    fixed a bug

--- numberGuessing.js
+++ numberGuessing.js
@@ -1,25 +1,26 @@
 const MAX = 100;
-const MIN = 50
+const MIN = 50;
+let attempts = 0;
 
-function checkNumber(guessedNumber , actualNumber){
-    if (guessedNumber > actualNumber) return 'Your guessed number is greater than the actual number'
-    else return  'Your guessed number is smaller than the actual number';
+function checkNumber(guessedNumber, actualNumber) {
+  if (guessedNumber > actualNumber)
+    return "Your guessed number is greater than the actual number";
+  else return "Your guessed number is smaller than the actual number";
 }
-const actualNumber = Math.floor(Math.random()*(MAX - MIN + MIN)) + MIN
-let guessedNumber = 0
+const actualNumber = Math.floor(Math.random() * (MAX - MIN) + MIN);
+let guessedNumber = 0;
 do {
-    guessedNumber = parseInt(prompt(`Guess a number between ${MIN} - ${MAX}`));
-  console.log(guessedNumber)
-  console.log(actualNumber)
+  guessedNumber = parseInt(prompt(`Guess a number between ${MIN} - ${MAX}`));
+  console.log(guessedNumber);
+  console.log(actualNumber);
   if (isNaN(guessedNumber)) {
     alert("Please enter a valid number");
   } else if (guessedNumber < MIN || guessedNumber > MAX) {
     alert(`The number should be between ${MIN} and ${MAX}. Please try again`);
   } else if (guessedNumber === actualNumber) {
-    alert("Congratulations! You got it right");
-  }
-  else{
+    alert("Congratulations! You got it right.It took you " + attempts + " attempts");
+  } else {
+    attempts++
     alert(checkNumber(guessedNumber, actualNumber));
   }
 } while (guessedNumber != actualNumber);
-
